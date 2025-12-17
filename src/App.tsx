8dce95220d2430910b5c569cc9b86437f541d979
Nusaiba-commits <nusa2559612@gmail.com>
2025-12-17 09:16:15 +0300

    Designed Responsive Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
-import { IconButton } from "@chakra-ui/react"
-import { LuSearch } from "react-icons/lu"
+import { Grid, GridItem } from "@chakra-ui/react";
+
 
 function App() {
-  return(
-    <IconButton aria-label="Search database">
-      <LuSearch />
-    </IconButton>
-  )
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`, //larger than 1024 px
+      }}
+    >
+      <GridItem area="nav" bg="red">
+        Nav
+      </GridItem>
+      <GridItem area="aside" bg="orange" display={{ base: "none", lg: "grid" }}>
+        Aside
+      </GridItem>
+      <GridItem area="main" bg="blue">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
-export default App
+export default App;
